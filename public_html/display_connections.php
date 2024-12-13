d648d130cac271288725a1d978ad3a7abf420b0c
Les Moulins du Dev <lesmoulinsdudev@gmail.com>
2024-12-13 21:16:26 +0100

    Sort result by date

--- public_html/display_connections.php
+++ public_html/display_connections.php
@@ -7,7 +7,7 @@ require_once("./../config.php");
 
 $db = new PDO("mysql:host=$DB_HOST:$DB_PORT;dbname=$DB_NAME", $DB_USER, $DB_PASS);
 
-$statement = $db->prepare("SELECT * FROM connections;");
+$statement = $db->prepare("SELECT * FROM connections ORDER BY `date` ASC;");
 $statement->execute();
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
